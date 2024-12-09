0314efe8f25b91cffaa2dff0529ff89d58d4cbfb
Mohit2005123 <mohitmongia2005@gmail.com>
2024-12-09 21:01:07 +0530

    Bug fix in prod 2

--- src/app/api/sendAdminEmail/route.js
+++ src/app/api/sendAdminEmail/route.js
@@ -1,31 +1,30 @@
 import nodemailer from 'nodemailer';
-const useremail='mohitmongia2005@gmail.com';
-const password='gpuc cyte sgva nmou';
+// const useremail='mohitmongia2005@gmail.com';
+// const password='gpuc cyte sgva nmou';
 export async function POST(request) {
   const { email, name, approvalLink, to } = await request.json();
-
+  const usermail=process.env.EMAIL_USER;
+  const userpass=process.env.EMAIL_PASS;
   try {
     // Configure Nodemailer
     const transporter = nodemailer.createTransport({
       service: 'gmail', // Or use another service like SendGrid
       auth: {
-        user: useremail, // Your email address
-        pass: password, // Your email password or app-specific password
+        user: usermail, // Your email address
+        pass: userpass, // Your email password or app-specific password
       },
     });
 
     // Email details
     const mailOptions = {
-      from: useremail,
-      to: useremail, // Your email address to receive notifications
+      from: usermail,
+      to: usermail, // Your email address to receive notifications
       subject: `Admin Signup Request: ${name}`,
       text: `A user (${name}, ${email}) has requested to sign up as an admin. Approve the request here: ${approvalLink}`,
     };
-    const testmail= process.env.EMAIL_USER;
-    const testpass= process.env.EMAIL_PASS;
     // Send email
     await transporter.sendMail(mailOptions);
-    return new Response(JSON.stringify({ success: true, testmail, testpass }), { status: 200 });
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
     console.error('Error sending email:', error);
     return new Response(JSON.stringify({ success: false, error }), { status: 500 });
